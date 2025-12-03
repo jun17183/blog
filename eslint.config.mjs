3d378ff3f42713b38a7404bf31d5b14e91b8a41a
jun17183 <jun17183@naver.com>
2025-12-03 16:06:33 +0900

    chore. prettier 적용

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -8,11 +10,25 @@ const eslintConfig = defineConfig([
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      // 싱글 쿼터 사용 강제
+      quotes: ['error', 'single', { avoidEscape: true }],
+      // JSX에서도 싱글 쿼터 사용
+      'jsx-quotes': ['error', 'prefer-single'],
+      // Prettier 규칙 적용
+      'prettier/prettier': 'error',
+    },
+  },
+  prettierConfig,
 ]);
 
 export default eslintConfig;
